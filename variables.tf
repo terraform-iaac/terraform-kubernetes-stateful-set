a354782d88dbfea7240d48d8027c3dbb8fd9aa59
bohdantverdyi <ownedd777@gmail.com>
2020-04-30 19:27:05 +0300

    GSIAAC-26 Add type for variables

--- variables.tf
+++ variables.tf
@@ -1,13 +1,17 @@
 variable "name" {
+  type = string
   description = "(Required) Deployment name"
 }
 variable "namespace" {
+  type = string
   description = "(Required) K8S namespace where deploy app"
 }
 variable "image" {
+  type = string
   description = "(Required) Docker image for app"
 }
 variable "volume_nfs" {
+  type = list(object({path_on_nfs = string, nfs_endpoint = string, volume_name = string}))
   description = "(Optional) Attach NFS"
   default = []
 }
@@ -16,6 +20,7 @@ variable "volume_host_path" {
   default = []
 }
 variable "volume_config_map" {
+  type = list(object({mode = string, name = string, volume_name = string}))
   description = "(Optional) The data stored in a ConfigMap object can be referenced in a volume of type configMap and then consumed by containerized applications running in a Pod"
   default = []
 }
@@ -28,14 +33,17 @@ variable "volume_mount" {
   default = []
 }
 variable "env" {
+  type = list(object({name = string, value = string}))
   description = "(Optional) Add environment variables to pods."
   default = []
 }
 variable "env_field" {
+  type = list(object({name = string, field_path = string}))
   description = "(Optional) Get field from k8s and add as environment variables to pods"
   default = []
 }
 variable "hosts" {
+  type = list(object({hostname = string, ip = string}))
   description = "(Optional) Add /etc/hosts records to pods"
   default = []
 }
@@ -52,42 +60,51 @@ variable "custom_label" {
   default = null
 }
 variable "args" {
+  type = list(string)
   description = "(Optional) Arguments to the entrypoint."
   default = []
 }
 variable "service_account_token" {
+  type = bool
   description = "Indicates whether a service account token should be automatically mounted"
   default = null
 }
 variable "service_account_name" {
+  type = string
   description = "(Optional) Is the name of the ServiceAccount to use to run this pod"
   default = null
 }
 variable "restart_policy" {
+  type = string
   description = "Restart policy for all containers within the pod. One of Always, OnFailure, Never"
   default = "Always"
 }
 variable "replicas" {
+  type = number
   description = "(Optional) Count of pods"
-  default = "1"
+  default = 1
 }
 variable "resources" {
   description = "(Optional) Limit resources by cpu or memory for pods"
   default = []
 }
 variable "pod_management_policy" {
+  type = string
   description = "(Optional) podManagementPolicy controls how pods are created during initial scale up, when replacing pods on nodes, or when scaling down"
   default = null
 }
 variable "revision_history_limit" {
+  type = number
   description = "(Optional) The maximum number of revisions that will be maintained in the StatefulSet's revision history"
   default = null
 }
 variable "update_strategy_type" {
+  type = string
   description = "(Optional) Indicates the type of the StatefulSetUpdateStrategy. There are two valid update strategies, RollingUpdate and OnDelete"
   default = "RollingUpdate"
 }
 variable "update_strategy_partition" {
+  type = string
   description = "(Optional) Indicates the ordinal at which the StatefulSet should be partitioned. You can perform a phased roll out (e.g. a linear, geometric, or exponential roll out) using a partitioned rolling update in a similar manner to how you rolled out a canary. To perform a phased roll out, set the partition to the ordinal at which you want the controller to pause the update. By setting the partition to 0, you allow the StatefulSet controller to continue the update process"
   default = "0"
 }
