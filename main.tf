ed8d8d3997e53809f19042f186805d2ffeb7028f
Bohdan Tverdyi <ownedd777@gmail.com>
2020-11-30 15:59:05 +0200

    FMT format

--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
 resource "kubernetes_stateful_set" "stateful_set" {
   metadata {
-    name = var.name
+    name      = var.name
     namespace = var.namespace
   }
   spec {
-    pod_management_policy = var.pod_management_policy
-    replicas = var.replicas
+    pod_management_policy  = var.pod_management_policy
+    replicas               = var.replicas
     revision_history_limit = var.revision_history_limit
-    service_name = var.name
+    service_name           = var.name
     selector {
       match_labels = {
         app = var.name
@@ -21,19 +21,19 @@ resource "kubernetes_stateful_set" "stateful_set" {
         dynamic "security_context" {
           for_each = var.security_context
           content {
-            fs_group = lookup(security_context.value, "fs_group", null)
-            run_as_group = lookup(security_context.value, "group_id", null)
+            fs_group        = lookup(security_context.value, "fs_group", null)
+            run_as_group    = lookup(security_context.value, "group_id", null)
             run_as_non_root = lookup(security_context.value, "as_non_root", null)
-            run_as_user = lookup(security_context.value, "user_id", null)
+            run_as_user     = lookup(security_context.value, "user_id", null)
           }
         }
-        service_account_name = var.service_account_name
+        service_account_name            = var.service_account_name
         automount_service_account_token = var.service_account_token
         container {
-          image = var.image
-          name = var.name
-          args = var.args
-          command = var.command
+          image             = var.image
+          name              = var.name
+          args              = var.args
+          command           = var.command
           image_pull_policy = var.image_pull_policy
           dynamic "env" {
             for_each = var.env_field
@@ -49,14 +49,14 @@ resource "kubernetes_stateful_set" "stateful_set" {
           dynamic "env" {
             for_each = var.env
             content {
-              name = env.value.name
+              name  = env.value.name
               value = env.value.value
             }
           }
           dynamic "env" {
             for_each = var.env_secret
             content {
-              name   = env.value.name
+              name = env.value.name
               value_from {
                 secret_key_ref {
                   name = env.value.secret_name
@@ -69,11 +69,11 @@ resource "kubernetes_stateful_set" "stateful_set" {
             for_each = var.resources
             content {
               requests {
-                cpu = lookup(resources.value, "request_cpu", null)
+                cpu    = lookup(resources.value, "request_cpu", null)
                 memory = lookup(resources.value, "request_memory", null)
               }
               limits {
-                cpu = lookup(resources.value, "limit_cpu", null)
+                cpu    = lookup(resources.value, "limit_cpu", null)
                 memory = lookup(resources.value, "limit_memory", null)
               }
             }
@@ -82,35 +82,35 @@ resource "kubernetes_stateful_set" "stateful_set" {
             for_each = var.internal_port
             content {
               container_port = port.value.internal_port
-              name = substr(port.value.name, 0, 14)
-              host_port = lookup(port.value, "host_port", null)
+              name           = substr(port.value.name, 0, 14)
+              host_port      = lookup(port.value, "host_port", null)
             }
           }
           dynamic "volume_mount" {
             for_each = var.volume_mount
             content {
               mount_path = volume_mount.value.mount_path
-              sub_path = lookup(volume_mount.value, "sub_path", null)
-              name = volume_mount.value.volume_name
-              read_only = lookup(volume_mount.value, "read_only", false)
+              sub_path   = lookup(volume_mount.value, "sub_path", null)
+              name       = volume_mount.value.volume_name
+              read_only  = lookup(volume_mount.value, "read_only", false)
             }
           }
           tty = "true"
         }
         node_selector = var.node_selector
-        dynamic "host_aliases"{
+        dynamic "host_aliases" {
           iterator = hosts
           for_each = var.hosts
           content {
             hostnames = [hosts.value.hostname]
-            ip = hosts.value.ip
+            ip        = hosts.value.ip
           }
         }
         dynamic "volume" {
           for_each = var.volume_nfs
           content {
             nfs {
-              path = volume.value.path_on_nfs
+              path   = volume.value.path_on_nfs
               server = volume.value.nfs_endpoint
             }
             name = volume.value.volume_name
@@ -121,7 +121,7 @@ resource "kubernetes_stateful_set" "stateful_set" {
           content {
             host_path {
               path = volume.value.path_on_node
-              type = lookup(volume.value, "type", null )
+              type = lookup(volume.value, "type", null)
             }
             name = volume.value.volume_name
           }
@@ -131,7 +131,7 @@ resource "kubernetes_stateful_set" "stateful_set" {
           content {
             config_map {
               default_mode = volume.value.mode
-              name = volume.value.name
+              name         = volume.value.name
             }
             name = volume.value.volume_name
           }
@@ -140,9 +140,9 @@ resource "kubernetes_stateful_set" "stateful_set" {
           for_each = var.volume_gce_disk
           content {
             gce_persistent_disk {
-              pd_name = volume.value.gce_disk
-              fs_type = lookup(volume.value, "fs_type", null )
-              partition = lookup(volume.value, "partition", null )
+              pd_name   = volume.value.gce_disk
+              fs_type   = lookup(volume.value, "fs_type", null)
+              partition = lookup(volume.value, "partition", null)
               read_only = lookup(volume.value, "read_only", null)
             }
             name = volume.value.volume_name
@@ -152,8 +152,8 @@ resource "kubernetes_stateful_set" "stateful_set" {
           for_each = var.volume_aws_disk
           content {
             aws_elastic_block_store {
-              fs_type = lookup(volume.value, "fs_type", null )
-              partition = lookup(volume.value, "partition", null )
+              fs_type   = lookup(volume.value, "fs_type", null)
+              partition = lookup(volume.value, "partition", null)
               read_only = lookup(volume.value, "read_only", null)
               volume_id = volume.value.volume_id
             }
--- variables.tf
+++ variables.tf
@@ -1,50 +1,50 @@
 variable "name" {
-  type = string
+  type        = string
   description = "(Required) Deployment name"
 }
 variable "namespace" {
-  type = string
+  type        = string
   description = "(Required) K8S namespace where deploy app"
 }
 variable "image" {
-  type = string
+  type        = string
   description = "(Required) Docker image for app"
 }
 variable "volume_nfs" {
-  type = list(object({path_on_nfs = string, nfs_endpoint = string, volume_name = string}))
+  type        = list(object({ path_on_nfs = string, nfs_endpoint = string, volume_name = string }))
   description = "(Optional) Attach NFS"
-  default = []
+  default     = []
 }
 variable "volume_host_path" {
   description = "(Optional) Attach a file or directory from the host node’s filesystem"
-  default = []
+  default     = []
 }
 variable "volume_config_map" {
-  type = list(object({mode = string, name = string, volume_name = string}))
+  type        = list(object({ mode = string, name = string, volume_name = string }))
   description = "(Optional) The data stored in a ConfigMap object can be referenced in a volume of type configMap and then consumed by containerized applications running in a Pod"
-  default = []
+  default     = []
 }
 variable "volume_gce_disk" {
   description = "(Optional) Create volume from google disk to pod"
-  default = []
+  default     = []
 }
 variable "volume_aws_disk" {
   description = "(Optional) Create volume from aws disk to pod"
-  default = []
+  default     = []
 }
 variable "volume_mount" {
   description = "(Optional) Mount path from pods to volume"
-  default = []
+  default     = []
 }
 variable "env" {
-  type = list(object({name = string, value = string}))
+  type        = list(object({ name = string, value = string }))
   description = "(Optional) Add environment variables to pods."
-  default = []
+  default     = []
 }
 variable "env_field" {
-  type = list(object({name = string, field_path = string}))
+  type        = list(object({ name = string, field_path = string }))
   description = "(Optional) Get field from k8s and add as environment variables to pods"
-  default = []
+  default     = []
 }
 variable "env_secret" {
   description = "(Optional) Get secret keys from k8s and add as environment variables to pods"
@@ -52,81 +52,81 @@ variable "env_secret" {
   default     = []
 }
 variable "hosts" {
-  type = list(object({hostname = string, ip = string}))
+  type        = list(object({ hostname = string, ip = string }))
   description = "(Optional) Add /etc/hosts records to pods"
-  default = []
+  default     = []
 }
 variable "internal_port" {
   description = "(Optional) Expose port in pods"
-  default = []
+  default     = []
 }
 variable "security_context" {
   description = "(Optional) Set startup user_id, when pods start"
-  default = []
+  default     = []
 }
 variable "custom_labels" {
   description = "(Optional) Add custom label to pods"
-  default = null
+  default     = null
 }
 variable "args" {
-  type = list(string)
+  type        = list(string)
   description = "(Optional) Arguments to the entrypoint."
-  default = []
+  default     = []
 }
 variable "command" {
   description = " (Optional) Entrypoint array. Not executed within a shell. "
-  default = []
+  default     = []
 }
 variable "service_account_token" {
-  type = bool
+  type        = bool
   description = "Indicates whether a service account token should be automatically mounted"
-  default = null
+  default     = null
 }
 variable "service_account_name" {
-  type = string
+  type        = string
   description = "(Optional) Is the name of the ServiceAccount to use to run this pod"
-  default = null
+  default     = null
 }
 variable "image_pull_policy" {
-  default = "IfNotPresent" // Always, Never or empty
+  default     = "IfNotPresent" // Always, Never or empty
   description = "he default pull policy is IfNotPresent which causes the Kubelet to skip pulling an image if it already exists. If you would like to always force a pull, you can do one of the following"
 }
 variable "restart_policy" {
-  type = string
+  type        = string
   description = "Restart policy for all containers within the pod. One of Always, OnFailure, Never"
-  default = "Always"
+  default     = "Always"
 }
 variable "replicas" {
-  type = number
+  type        = number
   description = "(Optional) Count of pods"
-  default = 1
+  default     = 1
 }
 variable "resources" {
   description = "(Optional) Limit resources by cpu or memory for pods"
-  default = []
+  default     = []
 }
 variable "pod_management_policy" {
-  type = string
+  type        = string
   description = "(Optional) podManagementPolicy controls how pods are created during initial scale up, when replacing pods on nodes, or when scaling down"
-  default = null
+  default     = null
 }
 variable "revision_history_limit" {
-  type = number
+  type        = number
   description = "(Optional) The maximum number of revisions that will be maintained in the StatefulSet's revision history"
-  default = null
+  default     = null
 }
 variable "update_strategy_type" {
-  type = string
+  type        = string
   description = "(Optional) Indicates the type of the StatefulSetUpdateStrategy. There are two valid update strategies, RollingUpdate and OnDelete"
-  default = "RollingUpdate"
+  default     = "RollingUpdate"
 }
 variable "update_strategy_partition" {
-  type = string
+  type        = string
   description = "(Optional) Indicates the ordinal at which the StatefulSet should be partitioned. You can perform a phased roll out (e.g. a linear, geometric, or exponential roll out) using a partitioned rolling update in a similar manner to how you rolled out a canary. To perform a phased roll out, set the partition to the ordinal at which you want the controller to pause the update. By setting the partition to 0, you allow the StatefulSet controller to continue the update process"
-  default = "0"
+  default     = "0"
 }
 variable "node_selector" {
   description = "(Optional) Specify node selector for pod"
-  type = map(string)
-  default = null
+  type        = map(string)
+  default     = null
 }
